9eba2b35ab39b09379e3ea823be759c43ccd150a
melbarch <elbarchany@gmail.com>
2019-11-07 17:11:46 +0100

    Fix eslint issues

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
-const exec = require("child_process").exec;
-const promisify = require("util").promisify;
+const exec = require('child_process').exec;
+const promisify = require('util').promisify;
 
 const asyncExec = promisify(exec);
 
 const getAllChromeProcesses = async () => {
   const command =
-    "wmic process where Caption='chrome.exe' get CommandLine,ProcessId /value";
+    'wmic process where Caption=\'chrome.exe\' get CommandLine,ProcessId /value';
   const result = await asyncExec(command);
   const stdout = result.stdout;
   const regex = /CommandLine=(.+)\s+ProcessId=(\d+)/g;
@@ -23,11 +23,11 @@ const getAllChromeProcesses = async () => {
 };
 
 const KillProcess = processList => {
-  const command = "taskkill /PID {ID} /F";
+  const command = 'taskkill /PID {ID} /F';
   processList.forEach(async processId => {
-    await asyncExec(command.replace("{ID}", processId))
+    await asyncExec(command.replace('{ID}', processId));
   });
-  console.log("Found [" + processList.length + "] chrome process");
+  console.log('Found [' + processList.length + '] chrome process');
 };
 
 (async () => {
